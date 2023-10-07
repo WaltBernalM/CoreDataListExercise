5be8d2a7e080f35b395e806d73ff7972ebcbc163
Walter Bernal <wltrbm@gmail.com>
2023-10-06 18:50:54 -0700

    Creation declaration of desserts to be added to the database

--- CoreDataListExercise/ContentView.swift
+++ CoreDataListExercise/ContentView.swift
@@ -35,7 +35,51 @@ struct ContentView: View {
     
     
     
-    func createDesserts() { 
+    func createDesserts() {
+        let profiterole = Dessert(context: viewContext)
+        profiterole.name = "Profiterole"
+        profiterole.size = "Large"
+        profiterole.price = 2.99
+        let cremeBrulee = Dessert(context: viewContext)
+        cremeBrulee.name = "Crème Burlée"
+        cremeBrulee.size = "Medium"
+        cremeBrulee.price = 3.99
+        let iceCream = Dessert(context: viewContext)
+        iceCream.name = "Ice Cream"
+        iceCream.size = "Extra Large"
+        iceCream.price = 5.99
+        let applePie = Dessert(context: viewContext)
+        applePie.name = "Apple Pie"
+        applePie.size = "Small"
+        applePie.price = 2.99
+        let rizALimperatrice = Dessert(context: viewContext)
+        rizALimperatrice.name = "Riz à l'impératrice"
+        rizALimperatrice.size = "Small"
+        rizALimperatrice.price = 2.99
+        let cheeseCake = Dessert(context: viewContext)
+        cheeseCake.name = "Cheese Cake"
+        cheeseCake.size = "Extra Small"
+        cheeseCake.price = 4.99
+        let carrotCake = Dessert(context: viewContext)
+        carrotCake.name = "Carrot Cake"
+        carrotCake.size = "Standard"
+        carrotCake.price = 3.99
+        let pieceMontee = Dessert(context: viewContext)
+        pieceMontee.name = "Pièce montée"
+        pieceMontee.size = "Medium"
+        pieceMontee.price = 5.99
+        let eclair = Dessert(context: viewContext)
+        eclair.name = "Éclair"
+        eclair.size = "Large"
+        eclair.price = 3.99
+        let crepeSuzette = Dessert(context: viewContext)
+        crepeSuzette.name = "Crêpe Suzette"
+        crepeSuzette.size = "Extra Large"
+        crepeSuzette.price = 3.99
+        saveToDatabase()
+    }
+    
+    func saveToDatabase() {
     }
     
 }
